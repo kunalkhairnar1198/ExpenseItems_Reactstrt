93c326cc81c65b8884be053d0df0c399d6005520
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2023-12-30 00:01:29 +0530

    adding style and varibale dynamically

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,29 @@
+import './ExpenseItem.css';
 
 function ExpenseItem() {
-  return( 
-    <div>
-        <h2>Expense Item!</h2>    
-        <ul>
-            <li>Food Rs 100</li>
-            <li>Petrol Rs 100</li>
-            <li>movie Rs 200</li>
-        </ul>
-  </div>
-  )
+
+  //add variable in jsx component it is use dynamically inside the javascript exprestion 
+  let expenseDate = new Date(2010, 10, 11);
+  let expenseTitle = 'Car Insurance';
+  let expenseAmount = 294.67;
+  let LocationOfExpenditure = 'where spend the money';
+
+  return (
+    //style
+    // this is not a html code it is a jsx code inside the javascript 
+    // they not use class keyword adding style into the jsx element because this class keyword is reserve keyword in javascript it is defining class in the oops in js instead of className
+    //it is use className in the cammelCase
+
+
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__location'>{LocationOfExpenditure}</div>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
