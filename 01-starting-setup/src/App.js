df7bca6b3312af49fcb323d2738e5ff7a3f16b23
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2023-12-30 01:24:42 +0530

    Map throgh call multiple functional comp call  and pass props child component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -34,11 +34,25 @@ function App() {
       <p>this also visible</p>
       {/* it will make reusable child component inside app.js component passing the props  */}
       {/* 4 function call inside the App compo and pass props expense data 4 times  */}
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} LocationOfExpenditure={expenses[0].LocationOfExpenditure} date={expenses[0].date}></ExpenseItem>
+      
+      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} LocationOfExpenditure={expenses[0].LocationOfExpenditure} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} LocationOfExpenditure={expenses[1].LocationOfExpenditure} date={expenses[1].date}></ExpenseItem>
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} LocationOfExpenditure={expenses[2].LocationOfExpenditure} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} LocationOfExpenditure={expenses[3].LocationOfExpenditure} date={expenses[3].date}></ExpenseItem>
-   
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} LocationOfExpenditure={expenses[3].LocationOfExpenditure} date={expenses[3].date}></ExpenseItem> */}
+ 
+   {/* it has same as previous functional call on each iteration call compo but different logic */}
+    {/* if you have ExpenseItem component multiple times for each expense */}
+    {/* use map function iterate over expenses array and dynamically generate the components */}
+     
+     {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id} 
+          title={expense.title}
+          amount={expense.amount}
+          LocationOfExpenditure={expense.LocationOfExpenditure}
+          date={expense.date}
+        />
+      ))}
 
     </div>
   );
