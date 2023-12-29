9547aec64d65cc1c0bc11db9a5ee5a5a2a701146
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2023-12-29 17:39:56 +0530

    first React commit add headers

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+
+function ExpenseItem() {
+  return( 
+    <div>
+        <h2>Expense Item!</h2>    
+        <ul>
+            <li>Food Rs 100</li>
+            <li>Petrol Rs 100</li>
+            <li>movie Rs 200</li>
+        </ul>
+  </div>
+  )
+}
+
+export default ExpenseItem;
