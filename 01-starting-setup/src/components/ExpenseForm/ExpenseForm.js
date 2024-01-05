99c939040ac8703ac3a911b9ee38db970e70dd31
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2024-01-05 12:09:20 +0530

    Form Submit button added and create when onsubmit click to updated state console it inside multiple state handler

--- 01-starting-setup/src/components/ExpenseForm/ExpenseForm.js
+++ 01-starting-setup/src/components/ExpenseForm/ExpenseForm.js
@@ -20,30 +20,36 @@ function ExpenseForm() {
         ...prevstate,
         Title: event.target.value,
       }))
-         console.log('it will imeediately execute',userInput)
-         console.log('targeted value i.e type value',event.target.value)
     }
     const handleAmount=(event)=>{
       setUserInput({
         ...userInput, //it holds prevstate object values create copy they will immediately executed
         amount :event.target.value, //when amount setUserInput fun call then userInput prev value will updated, when they will execute only pahili value
       })
-         console.log(userInput)
-         console.log(event.target.value)
     }
     const handleDate=(event)=>{
       setUserInput({
         ...userInput,
         Date :event.target.value,
       })
-         console.log(userInput)
-         console.log(event.target.value)
     }
-    
+    //add onSubmit Handler
+    let onSubmitHandler=(event)=>{
+      event.preventDefault()
+      // console.log(userInput.Title, userInput.amount, userInput.Date)
+      //it will store all targeted value in updatedState
+      //and print it
+      let updatedState={
+        Title:userInput.Title,
+        amount :userInput.amount,
+        Date : new Date(userInput.Date)
+      }
+      console.log(updatedState)
+    }
 
     return (
     <div className='new-expense'>
-        <form>
+        <form onSubmit={onSubmitHandler}>
              <h1>cretate form Expense And handle with vanila javascript</h1>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
@@ -61,7 +67,7 @@ function ExpenseForm() {
             </div>
         
             <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+                <button type='submit' >Add Expense</button>
             </div>
         </form>
     </div>
