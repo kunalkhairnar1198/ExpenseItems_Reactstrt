9b0518f19755908463d57292999426b7cc72bfd5
kunalkhairnar1198 <kunalkhairnar61@gmail.com>
2024-01-06 02:11:30 +0530

    Learn Two-way bindign controled component for form value hanlding

--- 01-starting-setup/src/components/ExpenseForm/ExpenseForm.js
+++ 01-starting-setup/src/components/ExpenseForm/ExpenseForm.js
@@ -1,71 +1,70 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css'
 
-function ExpenseForm() {
+function ExpenseForm(props) {
     
   //update the user input state in the form
-// const [enterTitle, setTitle] = useState('')
-// const [enterAmount, setAmount] = useState('')
-// const [enterDate, setDate] = useState('')
+const [enterTitle, setTitle] = useState('')
+const [enterAmount, setAmount] = useState('')
+const [enterDate, setDate] = useState('')
+const [enterExpenditure, setExpenditure] = useState('')
 
 
-    //how you can convert the multiple state into a single state.
-    const [userInput, setUserInput]= useState({
-      Title:'',
-      amount:'',
-      Date:'',
-    })
     const handleTitle=(event)=>{
-       // setTitle(event.target.value)  //piece of state update inside the function not multiple state handle
-      setUserInput((prevstate)=>({
-        ...prevstate,
-        Title: event.target.value,
-      }))
+       setTitle(event.target.value)  //piece of state update inside the function not multiple state handle
     }
     const handleAmount=(event)=>{
-       // setAmount(event.target.value)
-      setUserInput({
-        ...userInput, //it holds prevstate object values create copy they will immediately executed
-        amount :event.target.value, //when amount setUserInput fun call then userInput prev value will updated, when they will execute only pahili value
-      })
+       setAmount(event.target.value)
     }
     const handleDate=(event)=>{
-      // setDate(event.target.value)
-      setUserInput({
-        ...userInput,
-        Date :event.target.value,
-      })
+      setDate(event.target.value)
     }
+    const handleExpenditure=(event)=>{
+      setExpenditure(event.target.value)
+    }
+
     //add onSubmit Handler
     let onSubmitHandler=(event)=>{
       event.preventDefault()
-      // console.log(userInput.Title, userInput.amount, userInput.Date)
       //it will store all targeted value in updatedState
       //and print it
       let updatedState={
-        Title:userInput.Title,
-        amount :userInput.amount,
-        Date : new Date(userInput.Date)
+        title:enterTitle,
+        amount :enterAmount,
+        LocationOfExpenditure:enterExpenditure,
+        date : new Date(enterDate)
       }
-      console.log(updatedState)
+      console.log('store updated state in forms',updatedState)
+      //call the parent function and sent updated data in NewExpense parent Component
+      props.onSaveExpenseData(updatedState)
+
+      setTitle('')
+      setAmount('')
+      setDate('')
+      setExpenditure('')
+
     }
 
     return (
-    <div className='new-expense'>
+    
         <form onSubmit={onSubmitHandler}>
              <h1>cretate form Expense And handle with vanila javascript</h1>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={handleTitle}/>
+                    <input type='text' value={enterTitle} onChange={handleTitle}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' onChange={handleAmount} />
+                    <input type='number' value={enterAmount} onChange={handleAmount} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' onChange={handleDate}  />
+                    <input type='date' value={enterDate} onChange={handleDate}  />
+                </div>
+                <div className='new-expense__control'>
+                    <label>Expenditure</label>
+                    <input type='text' value={enterExpenditure} onChange={handleExpenditure}  />
                 </div>
             </div>
         
@@ -73,7 +72,7 @@ function ExpenseForm() {
                 <button type='submit' >Add Expense</button>
             </div>
         </form>
-    </div>
+    
   )
 }
 
